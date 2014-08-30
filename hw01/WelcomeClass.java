01945f9ea6ffe1d8dc6e2ee5dbd157892c83c340
maxhasselbusch <mah318@lehigh.edu>
2014-08-30 19:57:49 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+////////////////////////////////////
+//Maximilian Hasselbusch
+//August 27,2014
+//CSE2
+//Section 112
+
+// define a class
+public class WelcomeClass{
+ 
+// add main method
+  public static void main(String[] args) {
+
+// print the statement with box around it
+    System.out.println("  ------------      ");
+    System.out.println("  | WELCOME  |      ");
+    System.out.println("  ------------      ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-M--A--H--3--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("Over the summer, I visited Yosemite National Park");
+    System.out.println("and San Francisco with my family");
+    }
+}
